5467df4ae0e8bbc5a8b15f1132e04e62da585ecd
Lisa Jiamsirioungkul <visakhalisajiam@gmail.com>
2021-02-09 12:44:33 -0800

    form added

--- playlist-app/app.py
+++ playlist-app/app.py
@@ -14,10 +14,7 @@ db.create_all()
 
 app.config['SECRET_KEY'] = "I'LL NEVER TELL!!"
 
-# Having the Debug Toolbar show redirects explicitly is often useful;
-# however, if you want to turn it off, you can uncomment this line:
-#
-# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
 
@@ -44,20 +41,36 @@ def show_all_playlists():
 @app.route("/playlists/<int:playlist_id>")
 def show_playlist(playlist_id):
     """Show detail on specific playlist."""
-
-    # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
+    
+    playlist = Playlist.query.get_or_404(playlist_id)
+    return render_template('playlist.html', playlist=playlist)
 
 
 @app.route("/playlists/add", methods=["GET", "POST"])
 def add_playlist():
     """Handle add-playlist form:
-
     - if form not filled out or invalid: show form
     - if valid: add playlist to SQLA and redirect to list-of-playlists
     """
 
-    # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
+    form = PlaylistForm()
 
+    if form.validate_on_submit():
+        name = form.name.data
+        description = form.description.data
+        new_playlist = Playlist(name=name, description=description)
+        db.session.add(new_playlist)
+
+        try:
+            db.session.commit()
+        except IntegrityError as e:
+            if 'name' in e.orig.pgerror:
+                form.name.errors.append('Playlist name already exists. Please choose another.')
+                return render_template('new_playlist.html', form=form)
+
+        return redirect('/playlists')
+    
+    return render_template('new_playlist.html', form=form)
 
 ##############################################################################
 # Song routes
@@ -75,42 +88,54 @@ def show_all_songs():
 def show_song(song_id):
     """return a specific song"""
 
-    # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
-
+    song = Song.query.get_or_404(song_id)
+    return render_template('song.html', song=song)
 
 @app.route("/songs/add", methods=["GET", "POST"])
 def add_song():
     """Handle add-song form:
-
     - if form not filled out or invalid: show form
     - if valid: add playlist to SQLA and redirect to list-of-songs
     """
 
-    # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
+    form = SongForm()
+
+    if form.validate_on_submit():
+        title = form.title.data
+        artist = form.artist.data
+
+        if Song.query.filter(Song.title == title, Song.artist == artist).first():
+            form.title.errors.append('A song with that name and artist already exists in the database.')
+            return render_template('new_song.html', form=form)
+
+        new_song = Song(title=title, artist=artist)
+        db.session.add(new_song)
+        db.session.commit()
+
+        return redirect('/songs')
+
+    return render_template('new_song.html', form=form)
 
 
 @app.route("/playlists/<int:playlist_id>/add-song", methods=["GET", "POST"])
 def add_song_to_playlist(playlist_id):
     """Add a playlist and redirect to list."""
 
-    # BONUS - ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
-
-    # THE SOLUTION TO THIS IS IN A HINT IN THE ASSESSMENT INSTRUCTIONS
-
     playlist = Playlist.query.get_or_404(playlist_id)
     form = NewSongForPlaylistForm()
 
-    # Restrict form to songs not already on this playlist
-
-    curr_on_playlist = ...
-    form.song.choices = ...
+    curr_on_playlist = [s.id for s in playlist.songs]
+    form.song.choices = (db.session.query(Song.id, Song.title)
+                        .filter(Song.id.notin_(curr_on_playlist))
+                        .all())
 
     if form.validate_on_submit():
+        song = Song.query.get(form.song.data)
+        playlist.songs.append(song)
+        db.session.commit()
 
-          # ADD THE NECESSARY CODE HERE FOR THIS ROUTE TO WORK
-
-          return redirect(f"/playlists/{playlist_id}")
+        return redirect(f"/playlists/{playlist_id}")
 
     return render_template("add_song_to_playlist.html",
-                             playlist=playlist,
-                             form=form)
+                            playlist=playlist,
+                            form=form)
